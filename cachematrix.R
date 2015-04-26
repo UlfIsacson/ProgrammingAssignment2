a63d5daac5ab85c724a3fddf4eef60ce40d9fa6a
UlfIsacson <ulf.isacson@gmail.com>
2015-04-26 20:32:34 +0200

    My submit function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Create a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(solve) m <<- solve
+    getmatrix <- function() m
+    list(set=set, get=get,
+         setmatrix=setmatrix,
+         getmatrix=getmatrix)
 
 }
 
 
-## Write a short comment describing this function
 
+
+## Return matrix that is the inverse of 'x'
+## If its already been computed once, use cache
+## to return value. Print out message when chached
+## data is used
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+        message("return cached data...")
+        return(m)
+    }
+    
+    matrix <- x$get()
+    m <- solve(matrix, ...)
+    x$setmatrix(m)
+    m
+
 }
